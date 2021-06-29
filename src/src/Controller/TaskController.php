9e7da271a94a816839ea166997f4a7dbddf2e173
David Tobon <jdtobonf@gmail.com>
2021-06-28 22:45:56 -0500

    Added jwt authentication

---
+++ src/src/Controller/TaskController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api")
+ */
+class TaskController extends AbstractController
+{
+    /**
+     * @Route("/task", name="task")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/TaskController.php',
+        ]);
+    }
+}
